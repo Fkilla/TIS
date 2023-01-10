ea8de693534d6808e723ab9b103596b43a88d572
kimyujin <jjks20007@daum.net>
2023-01-10 23:01:02 +0900

    2023.01.10 23:00

--- test/src/main/java/org/zerock/controller/BoardController.java
+++ test/src/main/java/org/zerock/controller/BoardController.java
@@ -1,7 +1,5 @@
 package org.zerock.controller;
 
-import jdk.internal.org.jline.utils.Log;
-
 public class BoardController {
 	public void board() {
 		System.out.println("test~~~~~~~~~~~~~~~!!!!!!!!");
